f63ee77b54ac4121725e45a49780a17cf87f1565
byskoro <byskoro@gmail.com>
2018-11-06 23:49:58 +0200

    ch

---
+++ Assets/_Scripts/Init.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : MonoBehaviour {
+
+	public GameObject connectMenu;
+	public GameObject gamesMenus;
+
+	// Use this for initialization
+	void Start () {
+
+		if (ThinkGearController.Instance.poorSignalStatus == 0) {
+			connectMenu.SetActive (true);
+			gamesMenus.SetActive (false);
+		} else {
+			connectMenu.SetActive (false);
+			gamesMenus.SetActive (true);
+		}
+	}
+}
--- Assets/_Scripts/MainMenu/GamesMenu.cs
+++ Assets/_Scripts/MainMenu/GamesMenu.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GamesMenu : MonoBehaviour {
 
-	// Use this for initialization
+	public GameObject connectMenu;
+	public GameObject gamesMenus;
+	public Texture2D[] signalIcons;
+
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (ThinkGearController.Instance.poorSignalStatus == 0)
+		{
+			connectMenu.SetActive (true);
+			gamesMenus.SetActive (false);
+		}
+				
+	}
+
+	public void LoadTimeGame()
+	{
+		SceneManager.LoadScene("ClockScene");
+	}
+	public void LoadEarthGame()
+	{
+		SceneManager.LoadScene("EarthScene");
+	}
+
+	void OnGUI()
+	{
+		GUILayout.BeginHorizontal();
+		GUILayout.Space(Screen.width - 35);
+		GUILayout.Label(signalIcons[0]);
+		GUILayout.EndHorizontal();
 	}
 }
