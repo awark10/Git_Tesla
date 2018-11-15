e2e40f6bb30970f30f14e3a18784ef3d1153e6c1
awark10 <awark10@gmail.com>
2018-11-15 16:09:24 +0200

    tesla

---
+++ Assets/_Scripts/Earth/Nikolo_Tesla_animationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nikolo_Tesla_animationController : MonoBehaviour {
+
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        animator.SetInteger("nikoloState", 0);
+
+    }
+	
+    public void Moving()
+    {
+        animator.SetInteger("nikoloState", 1);
+    }
+    public void IdleStay()
+    {
+        animator.SetInteger("nikoloState", 0);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
