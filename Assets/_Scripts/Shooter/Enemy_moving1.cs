4ad5dbecf5c68737b0465247cd03b63b87a1c03a
awark10 <awark10@gmail.com>
2018-12-01 16:02:41 +0200

    234

--- Assets/_Scripts/Shooter/Enemy_moving1.cs
+++ Assets/_Scripts/Shooter/Enemy_moving1.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Enemy_moving1 : MonoBehaviour {
 
     public float speed;
+    public float direction;
     // Use this for initialization
     void Start () {
-		
+        direction = GetComponent<Transform>().localScale.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (direction>0)
         transform.Translate(Vector3.right * speed * Time.deltaTime);
+        else transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
 }
