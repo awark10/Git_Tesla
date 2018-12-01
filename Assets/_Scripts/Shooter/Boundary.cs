941e78915b27c7a06a147806361270b908fe593a
awark10 <awark10@gmail.com>
2018-12-01 11:39:57 +0200

    duck animation add

---
+++ Assets/_Scripts/Shooter/Boundary.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour
+{
+
+    BoxCollider2D boundareCollider;
+
+    //receiving collider's component and changing boundary borders
+    private void Start()
+    {
+        boundareCollider = GetComponent<BoxCollider2D>();
+        ResizeCollider();
+    }
+
+    //changing the collider's size up to Viewport's size multiply 1.5
+    void ResizeCollider()
+    {
+        Vector2 viewportSize = Camera.main.ViewportToWorldPoint(new Vector2(1, 1)) * 2;
+        viewportSize.x *= 1.5f;
+        viewportSize.y *= 1.5f;
+        boundareCollider.size = viewportSize;
+    }
+
+    //when another object leaves collider
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+       
+            Destroy(collision.gameObject);
+        
+    }
+
+}
