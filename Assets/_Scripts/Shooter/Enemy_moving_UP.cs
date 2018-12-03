ac67942c0863f05b7d9f0f9573087ff92fe0c00c
awark10 <awark10@gmail.com>
2018-12-03 17:34:11 +0200

    321

---
+++ Assets/_Scripts/Shooter/Enemy_moving_UP.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Enemy_moving_UP : MonoBehaviour {
+
+    public float speed;
+   
+    void Update()
+    {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        transform.localScale -= (new Vector3(0.2f, 0.2f, 0.2f) * speed * Time.deltaTime);
+
+
+    }
+}
