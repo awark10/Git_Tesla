9043ce3c3bbb0fad24f6911e786bcad955ca95ef
awark10 <awark10@gmail.com>
2018-12-08 11:26:01 +0200

    add new enemy

---
+++ Assets/_Scripts/Shooter/Enemy_moving2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_moving2 : MonoBehaviour {
+    public float speed;
+    public float direction;
+    public Vector3 rightMov ;
+    public Vector3 leftMov;
+    // Use this for initialization
+    void Start()
+    {
+        direction = GetComponent<Transform>().localScale.x;
+        rightMov = new Vector3(1, 1, 0);
+     leftMov = new Vector3(-1, 1, 0);
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction > 0)
+            transform.Translate(rightMov * speed * Time.deltaTime);
+        else transform.Translate(leftMov * speed * Time.deltaTime);
+    }
+}
