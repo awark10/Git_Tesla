aa15c172ac0a72a9da891bd3ed92f9e9cfb96503
byskoro <byskoro@gmail.com>
2018-12-21 13:47:23 +0200

    ch

--- Assets/_Scripts/Earth/Earth_Statistic_P1.cs
+++ Assets/_Scripts/Earth/Earth_Statistic_P1.cs
@@ -5,95 +5,37 @@ using UnityEngine;
 
 public class Earth_Statistic_P1 : MonoBehaviour {
 
-	int Meditation, Attention;
-
-	public float attQual, medQual;
-	public int attAvg,  medAvg;
-
-	public int medMinutes, medSeconds;
-	public int attMinutes, attSeconds;
-	public int gameMinutes, gameSeconds;
-	public float msecs = 0;
-
+	public int avgMeditation, avgAttention;
+	public float deepMeditationTime, deepAttentionTime;
 	public int ballsVal;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () 
 	{		
 		if (CONNECTOR.Instance.Attention > 0 && CONNECTOR.Instance.Meditation > 0) 
 		{
-			Attention = CONNECTOR.Instance.Attention;
-			Meditation = CONNECTOR.Instance.Meditation;
-			AvgTime();
-			Quality();
+			AvgLevel();
+			DeepLevel ();
+			balls();
 		}		
 	}
 
-	void Quality()
+	void AvgLevel()
 	{
-		float totalTime = gameMinutes * 60 + gameSeconds;
-		float medTime   = medMinutes * 60 + medSeconds;
-		float attTime   = attMinutes * 60 + attSeconds;
-
-		attQual = (attTime / totalTime) * 100;
-		medQual = (medTime / totalTime) * 100;
+		avgMeditation = ( avgMeditation + CONNECTOR.Instance.Meditation ) / 2;
+		avgAttention  = ( avgAttention + CONNECTOR.Instance.Attention ) / 2;
 	}
 
-	void AvgTime()
+	void DeepLevel ()
 	{
-		msecs += Time.deltaTime;
-
-		if (msecs >= 1.0f)
-		{
-			msecs = 0;
-			gameSeconds++;
-
-			if (gameSeconds > 59)
-			{
-				gameSeconds = 0;
-				gameMinutes++;
-			}
-
-			if (Meditation > 50) {
-
-				medSeconds++;
-
-				if (medSeconds > 59) {
-					medMinutes++;
-					medSeconds = 0;
-				}
-			}
-
-			if (Attention > 50) {
-			
-				attSeconds++;
-
-				if (attSeconds > 59) {
-					attMinutes++;
-					attSeconds = 0;
-				}
-			}
-
-			balls ();
-		}			
+		if (CONNECTOR.Instance.Meditation > 80)
+			deepMeditationTime += Time.deltaTime;
+		if (CONNECTOR.Instance.Attention > 80)
+			deepAttentionTime += Time.deltaTime;
 	}
 
+	void balls()
+	{
 
-	void balls(){
-
-		if (Meditation > 50 && Attention > 50)
-			ballsVal += 1;
-		if (Meditation > 60 && Attention > 60)
-			ballsVal += 2;
-		if (Meditation > 70 && Attention > 70)
-			ballsVal += 3;
-		if (Meditation > 80 && Attention > 80)
-			ballsVal += 5;
 	}
-
 }
