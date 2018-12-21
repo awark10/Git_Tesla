08fc64da767370ec42af4d8b4067fc5592016842
awark10 <awark10@gmail.com>
2018-12-21 15:50:26 +0200

    123123

--- Assets/_Scripts/Clock/TimeStatistic.cs
+++ Assets/_Scripts/Clock/TimeStatistic.cs
@@ -1,16 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeStatistic : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public ClockGame clockGame;
+
+    public int avgMeditation, avgAttention;
+    public float deepMeditationTime, deepAttentionTime;
+    public int ballsVal;
+    public Text avgMedText, avgAttText, deepAttText, deepMedText, gpText, descriptionGameText;
+
+    private void Start()
+    {
+        descriptionGameText.text = "текст описани для игр, условий уровней и тд";
+    }
+
+    void Update()
+    {
+        if (CONNECTOR.Instance.Attention > 0 && CONNECTOR.Instance.Meditation > 0)
+        {
+            AvgLevel();
+            DeepLevel();
+            Balls();
+        }
+    }
+
+    void AvgLevel()
+    {
+        avgMeditation = (avgMeditation + CONNECTOR.Instance.Meditation) / 2;
+        avgAttention = (avgAttention + CONNECTOR.Instance.Attention) / 2;
+        avgMedText.text = "" + avgMeditation;
+        avgAttText.text = "" + avgAttention;
+    }
+
+    void DeepLevel()
+    {
+        if (CONNECTOR.Instance.Meditation > 80)
+            deepMeditationTime += Time.deltaTime;
+
+        if (CONNECTOR.Instance.Attention > 80)
+            deepAttentionTime += Time.deltaTime;
+        deepMedText.text = "" + (int)deepMeditationTime + " сек";
+        deepAttText.text = "" + (int)deepAttentionTime + " сек";
+
+    }
+
+    void Balls()
+    {
+        ballsVal = (avgMeditation + avgAttention);
+        gpText.text = "" + ballsVal;
+
+    }
 }
