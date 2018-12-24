6c02d1107513d4278a66354d1b6450cba5a58d43
byskoro <byskoro@gmail.com>
2018-12-24 15:30:38 +0200

    ch

--- Assets/_Scripts/Bak/BacGame.cs
+++ Assets/_Scripts/Bak/BacGame.cs
@@ -11,6 +11,11 @@ public class BacGame : MonoBehaviour {
 
 	int [] ladLevel = {0,15,30,45,60,75,90,100};
 	float [,] DeltaSoundArray = { {0f,10f}, {11.2f,20f}, {21.4f,31f}, {33.6f,45f}, {47.3f,59f}, {60.8f,73f}, {74.8f,84f} };
+	float [,] ThetaSoundArray = { {1f,6f}, {7.5f,13f}, {14.5f,18f}, {19.2f,24f}, {24.7f,30f}, {31.6f,36f}, {37.6f,42.6f} };
+	float [,] AlphaHSoundArray = { {0f,4.2f}, {4.5f,11.3f}, {11.7f,18.9f}, {19.4f,26.5f}, {27.2f,31.6f}, {31.9f,37.2f}, {37.7f,43.2f} };
+	float [,] AlphaLSoundArray = { {2.3f,6.3f}, {6.7f,10.6f}, {11.3f,15.4f}, {16.3f,21.0f}, {22.0f,26.0f}, {29.4f,33.0f}, {35.0f,40.0f} };
+	float [,] BetaHSoundArray = { {2.3f,6.3f}, {6.7f,10.6f}, {11.3f,15.4f}, {16.3f,21.0f}, {22.0f,26.0f}, {29.4f,33.0f}, {35.0f,40.0f} };
+
 
 	void Start () {
 
@@ -48,10 +53,16 @@ public class BacGame : MonoBehaviour {
 			bacDelta.play (data.Delta, data.avgDelta, ladLevel, DeltaSoundArray);
 
 		if (data.getIsNewTheta()) 
-			bacTheta.play (data.Theta, data.avgTheta, ladLevel, DeltaSoundArray);
+			bacTheta.play (data.Theta, data.avgTheta, ladLevel, ThetaSoundArray);
 
 		if (data.getIsNewHighAlpha()) 
-			bacAlphaH.play (data.HighAlpha, data.HighAlpha, ladLevel, DeltaSoundArray);
+			bacAlphaH.play (data.HighAlpha, data.HighAlpha, ladLevel, AlphaHSoundArray);
+
+		if (data.getIsNewLowAlpha()) 
+			bacAplhaL.play (data.LowAlpha, data.LowAlpha, ladLevel, AlphaHSoundArray);
+
+		if (data.getIsNewHighBeta()) 
+			bacBetaH.play (data.HighBeta, data.HighBeta, ladLevel, BetaHSoundArray);
 	}
 
 	void UIupdate()
