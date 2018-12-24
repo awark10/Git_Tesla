7b8f3442e48993fe3729faa400727b53d675bd14
byskoro <byskoro@gmail.com>
2018-12-24 16:39:51 +0200

    ch

--- Assets/_Scripts/Bak/BacGame.cs
+++ Assets/_Scripts/Bak/BacGame.cs
@@ -14,9 +14,10 @@ public class BacGame : MonoBehaviour {
 	float [,] ThetaSoundArray = { {1f,6f}, {7.5f,13f}, {14.5f,18f}, {19.2f,24f}, {24.7f,30f}, {31.6f,36f}, {37.6f,42.6f} };
 	float [,] AlphaHSoundArray = { {0f,4.2f}, {4.5f,11.3f}, {11.7f,18.9f}, {19.4f,26.5f}, {27.2f,31.6f}, {31.9f,37.2f}, {37.7f,43.2f} };
 	float [,] AlphaLSoundArray = { {2.3f,6.3f}, {6.7f,10.6f}, {11.3f,15.4f}, {16.3f,21.0f}, {22.0f,26.0f}, {29.4f,33.0f}, {35.0f,40.0f} };
-	float [,] BetaHSoundArray = { {1.5f,10.8f}, {12.5f,20.1f}, {21.3f,28.2f}, {29.2f,36.6f}, {37.9f,45.2f}, {45.9f,52.7f}, {53.9f,60.0f} };
-	float [,] BetaLSoundArray = { {1.4f,6.2f}, {7.0f,12.3f}, {13.2f,18.2f}, {19.2f,24.2f}, {25.1f,29.4f}, {30.1f,35.5f}, {36.1f,41.0f} };
-	float [,] GammaHSoundArray = { {1.4f,6.2f}, {7.0f,12.3f}, {13.2f,18.2f}, {19.2f,24.2f}, {25.1f,29.4f}, {30.1f,35.5f}, {36.1f,41.0f} };
+	float [,] BetaHSoundArray = { {1.5f,67.0f}, {12.5f,67.0f}, {21.3f,67.0f}, {29.2f,67.0f}, {37.9f,67.0f}, {45.9f,67.0f}, {53.9f,67.0f} };
+	float [,] BetaLSoundArray = { {1.4f,42.0f}, {7.0f,42.0f}, {13.2f,42.0f}, {19.2f,42.0f}, {25.1f,42.0f}, {30.1f,42.0f}, {36.1f,42.0f} };
+	float [,] GammaLSoundArray = { {9.5f,160.0f}, {21.4f,160.2f}, {30.8f,160.2f}, {39.0f,160.3f}, {50.0f,160.0f}, {78.5f,160.0f}, {94.6f,160.0f} };
+	float [,] GammaHSoundArray = { {30.0f,480.0f}, {51.0f,480.2f}, {74.0f,480.2f}, {110.0f,480.3f}, {260.0f,480.0f}, {280.5f,480.0f}, {305.6f,480.0f} };
 
 	void Start () {
 
@@ -67,6 +68,9 @@ public class BacGame : MonoBehaviour {
 
 		if (data.getIsNewLowBeta()) 
 			bacBetaL.play (data.LowBeta, data.avgLowBeta, ladLevel, BetaLSoundArray);
+
+		if (data.getIsNewLowGamma()) 
+			bacGammaL.play (data.LowGamma, data.avgLowGamma, ladLevel, GammaLSoundArray);
 
 		if (data.getIsNewHighGamma()) 
 			bacGammaH.play (data.HighGamma, data.avgHighGamma, ladLevel, GammaHSoundArray);
